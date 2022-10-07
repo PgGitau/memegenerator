42b55ee7372811e6a8845fa0ce48ab4f8c33ba6b
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-10-07 08:41:03 +0300

    edit form and its styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,11 +3,11 @@ import './Form.css';
 function Form() {
     return(
         <div className="form-container">
-            <form className="form" name='memeform' method='post' action='#'>
+            <div className="form" name='memeform' method='post' action='#'>
                 <input className="form-input input1" type="text" placeholder="First text" name="first" id="first"></input>
                 <input className="form-input input2" type="text" placeholder="Second text" name="last" id="last"></input>
                 <button className="form-button">Get a new meme image 🖼</button>
-            </form>
+            </div>
         </div>
     )
 }
