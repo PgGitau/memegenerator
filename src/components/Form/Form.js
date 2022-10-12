b54898a04492a9a7b3195cc660d0d395d6cc897b
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-10-12 19:56:18 +0300

    edit form to enable inputed text to appear on image  and its styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,81 @@
 import './Form.css';
+import {useState} from 'react';
+import MemesData from '../../MemesData';
 
 function Form() {
-    return(
-        <div className="form-container">
-            <div className="form" name='memeform' method='post' action='#'>
-                <input className="form-input input1" type="text" placeholder="First text" name="first" id="first"></input>
-                <input className="form-input input2" type="text" placeholder="Second text" name="last" id="last"></input>
-                <button className="form-button">Get a new meme image 🖼</button>
-            </div>
+    const [ memeData, setMemeData ] = useState(
+        {
+           firstText: "",
+           secondText: "",
+           randomImage: "" 
+        }
+    );
+
+    function getMemeImage() {
+        const imgArray = MemesData.data.memes;
+        const num = Math.floor(Math.random() * imgArray.length)
+        const memeImg = imgArray[num].url;
+        
+        setMemeData(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: memeImg
+            }
+        } )
+    }
+
+    
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setMemeData(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
+
+    return (
+      <div className="form-container">
+        <div className="form" name="memeform" method="post" action="#">
+          <input
+            className="form-input input1"
+            type="text"
+            placeholder="First text"
+            name="firstText"
+            id="first"
+            onChange={handleChange}
+            value={memeData.firstText}
+          >
+          </input>
+
+          <input
+            className="form-input input2"
+            type="text"
+            placeholder="Second text"
+            name="secondText"
+            id="last"
+            onChange={handleChange}
+            value={memeData.secondText}
+          >
+          </input>
+
+          <button
+            className="form-button"
+            onClick={getMemeImage}
+          >
+            Get a new meme image 🖼
+          </button>
+
         </div>
-    )
+        <div className="meme">
+            <img src={memeData.randomImage} className="meme--image" alt="random meme"/>
+            <h2 className="meme--text top">{memeData.firstText}</h2>
+            <h2 className="meme--text bottom">{memeData.secondText}</h2>
+        </div>
+      </div>
+    );
 }
 
 export default Form;
